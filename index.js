e4f47514d88fc6e2489c3e0ffa979b00104d591a
aiste334 <aistekst344@gmail.com>
2023-10-05 11:50:45 +0200

    All done!

--- index.js
+++ index.js
@@ -5,17 +5,31 @@ const jwt = require("jsonwebtoken");
 const bcrypt = require("bcryptjs");
 const express = require("express")
 const bodyParser = require("body-parser")
+const cookieParser = require("cookie-parser")
 const mongoose = require("mongoose")
 const cors = require("cors")
-const auth = require("./auth.js")
 
 const PORT = 3009
 const URI = "mongodb://127.0.0.1:27017/trippin"
 
 const app = express()
 app.use(bodyParser.json())
+app.use(cookieParser());
 app.use(cors())
 
+const authorization = (req, res, next) => {
+  const token = req.cookies.access_token;
+  if (!token) {
+    return res.sendStatus(403);
+  }
+  try {
+    const data = jwt.verify(token, process.env.TOKEN_KEY);
+    req.userId = data.id;
+    return next();
+  } catch {
+    return res.sendStatus(403);
+  }
+};
 
 //Destinations
 
@@ -49,7 +63,7 @@ app.put("/destinations/:id", async (req, res) => {
   }
 })
 
-app.delete("/destinations/:id", auth.checkLogin, async (req, res) => {
+app.delete("/destinations/:id", authorization, async (req, res) => {
   try {
     await Destination.deleteOne({ _id: req.params.id })
     res.status(204).json({ message: "Deleted successfully" })
@@ -59,12 +73,16 @@ app.delete("/destinations/:id", auth.checkLogin, async (req, res) => {
 })
 
 
-app.get("/user", async (req, res) => {
+app.get("/user", authorization, async (req, res) => {
   try {
     const user = await User.findOne({username: req.body.username})
     res.status(200).json(user)
-  } catch (err) {}
+  } catch (err) {
+    res.status(500).json({ message: "something didn't work" })
+
+  }
 })
+
 
 // Authentication controllers
 
@@ -77,19 +95,14 @@ app.post("/register", async (req, res) => {
     try {
       req.body.password = await bcrypt.hash(req.body.password, 10);
       const user = await User.create(req.body)
-      console.log("eeee")
       res.status(201).json(user)
     } catch (err) {
       console.log(err)
       res.status(500).json({ message: `Failed to create user: ${err}` })
     }
   } else {
-    
-  console.log("aaaa")
     res.status(400).send({ message: "Failed! Username is already in use!" });
   }
-  
-  console.log("dddd")
 })
 
 //login
@@ -114,8 +127,13 @@ app.post("/login", async (req, res) => {
 
       //Save user token
       user.token = token;
-      console.log(token);
-        res.status(201).json({ token });
+        res
+        .cookie("access_token", token, {
+          httpOnly: true,
+          secure: false,
+        })
+        .status(201)
+        .json({ token: token, message: "Logged in successfully" });
       } else {
         res.status(400).json({ error: "password doesn't match" });
       }
@@ -126,6 +144,13 @@ app.post("/login", async (req, res) => {
     console.log(error)
     res.status(400).json({ error});
   }
+});
+
+app.get("/logout", authorization, (req, res) => {
+  return res
+    .clearCookie("access_token")
+    .status(200)
+    .json({ message: "Successfully logged out" });
 });
 
 app.listen(PORT, () => {
